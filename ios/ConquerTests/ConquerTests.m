8abe221496a51fdb10bc088cd4facfb4afaaa401
awesssome-pro <awessssomepro@gmail.com>
2021-10-31 13:04:30 +0530

    renamed the app successfully

--- ios/ConquerTests/ConquerTests.m
+++ ios/ConquerTests/ConquerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface conquer_appTests : XCTestCase
+@interface ConquerTests : XCTestCase
 
 @end
 
-@implementation conquer_appTests
+@implementation ConquerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
