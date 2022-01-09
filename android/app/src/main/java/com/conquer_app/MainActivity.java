a528830d726653f1b1467e384d600a28ef7001be
awesssome-pro <awessssomepro@gmail.com>
2022-01-09 23:28:27 +0530

    successfully logging which app is running in the present

--- android/app/src/main/java/com/conquer_app/MainActivity.java
+++ android/app/src/main/java/com/conquer_app/MainActivity.java
@@ -2,12 +2,16 @@ package com.conquer_app;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -29,6 +33,29 @@ public class MainActivity extends ReactActivity {
         super.onCreate(null);
         createNotificationChannel("task_reminders", "Task Reminders", "This channel handles all notifications regarding task reminders", NotificationManager.IMPORTANCE_MAX);
         createNotificationChannel("foreground_services", "Foreground Service", "This channel handles that annoying notification which can't be turned off due to some fucking Android Policy", NotificationManager.IMPORTANCE_NONE);
+        if (!checkAccessibilityPermission()) {
+            Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+        Log.d("obscure_tag", "application has started!");
+    }
+
+    public boolean checkAccessibilityPermission() {
+        int accessEnabled = 0;
+        try {
+            accessEnabled = Settings.Secure.getInt(this.getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (accessEnabled == 0) {
+            // if not construct intent to request permission
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            // request permission via start activity for result
+            startActivity(intent);
+            return false;
+        } else {
+            return true;
+        }
     }
 
     private void createNotificationChannel(String channel_id, String channel_name, String channel_description, int channel_importance) {
