d7751c4c7f9d0daf90a962b65cc8858e91ba7a0b
devout-coder <awessssomepro@gmail.com>
2022-02-26 20:11:39 +0530

    users can now send friend requests to others

--- pages/Friends.js
+++ pages/Friends.js
@@ -1,7 +1,8 @@
-import {StyleSheet, Text, View, BackHandler} from 'react-native';
+import {StyleSheet, Text, View, BackHandler, Share} from 'react-native';
 import React, {useEffect} from 'react';
 import globalStyles from '../globalStyles';
 import Ripple from 'react-native-material-ripple';
+import auth from '@react-native-firebase/auth';
 
 const Friends = ({navigation}) => {
   useEffect(() => {
@@ -16,6 +17,38 @@ const Friends = ({navigation}) => {
       backHandler.remove();
     };
   });
+
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        // title: `Tap this link to accept ${
+        //   auth().currentUser.displayName
+        // }'s friend request\nhttps://conquer-goals.netlify.app/add-friend/${
+        //   auth().currentUser.uid
+        // }`,
+        message: `Tap this link to accept ${
+          auth().currentUser.displayName
+        }'s Conquer friend request\nhttps://conquer-goals.netlify.app/add-friend/${
+          auth().currentUser.uid
+        }`,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+          console.log('shared with activity type of result.activityType');
+        } else {
+          // shared
+          console.log('shared');
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+        console.log('dismissed');
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <View style={globalStyles.overallBackground}>
       <View style={styles.mainContainer}>
@@ -25,6 +58,7 @@ const Friends = ({navigation}) => {
         <Ripple
           rippleDuration={300}
           rippleColor="#ffffff"
+          onPress={onShare}
           rippleContainerBorderRadius={5}>
           <Text style={styles.shareLink}>Share friendship link</Text>
         </Ripple>
